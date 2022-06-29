461a32f95f4f2e6f82173b5db35dbabf3873e7f9
tiagosansopa <tiagosansopa@gmail.com>
2022-06-29 17:47:43 -0600

    New changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <header>UIM Santiago Solórzano</header>
+      <div className="about-title">Who am I?</div>
+      <div className="about">
+        Avid developer with more than 5 years experience. Worked on different
+        projects from mobile app development to full web applications.
+      </div>
+      <div className="about-title">What I like?</div>
+      <div className="about">
+        I enjoy being social. Share with people and interact with different
+        cultures. Learn by listening from people experiences. I also enjoy
+        manual art projects. Learning is another passion of mine. I enjoy
+        lectures from different topics. Specially from computer science.
+      </div>
+      <div className="about-title">Experience</div>
+      <div className="about">
+        Mobile development. Android Web development. Java, Vue, Javascript,
+        Node, React
+      </div>
+      <img></img>
+      <img></img>
     </div>
   );
 }
---
+++ src/Components/Name.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Name = () => {
+  return <div></div>;
+};
+
+export default Name;
