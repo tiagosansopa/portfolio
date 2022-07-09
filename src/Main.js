02454cb06e177b617d8f1413ed255eb49a557e93
tiagosansopa <tiagosansopa@gmail.com>
2022-07-09 14:28:38 -0600

    background

--- src/Main.js
+++ src/Main.js
@@ -4,22 +4,6 @@ const Main = () => {
   return (
     <div>
       <header>uim</header>
-      <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
-        <iframe
-          src="https://giphy.com/embed/h2hquAn8uv8SA"
-          width="100%"
-          height="100%"
-          style="position:absolute"
-          frameBorder="0"
-          class="giphy-embed"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <p>
-        <a href="https://giphy.com/gifs/zac-efron-funny-dancing-h2hquAn8uv8SA">
-          via GIPHY
-        </a>
-      </p>
     </div>
   );
 };
