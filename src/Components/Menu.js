c77eacaf06fd4d926a2e48cdbff0151501fdc824
tiagosansopa <tiagosansopa@gmail.com>
2022-07-10 20:16:44 -0600

    start of sections

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Menu.css";
 
-const Menu = () => {
-  return <div></div>;
+export const Menu = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            UIM
+            <i class="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Samples
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Exp
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Reach Out
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <button buttonStyle="btn--outline">SIGN UP</button>}
+        </div>
+      </nav>
+    </div>
+  );
 };
-
-export default Menu;
---
+++ src/Sections/Cover.js
@@ -0,0 +1,16 @@
+import React from "react";
+import z from "./z.jpg";
+import "Cover.css";
+export const Cover = () => {
+  return (
+    <div className="cover">
+      <div>
+        <img src={z}></img>
+        <h3>UIM</h3>
+        <h2>Santiago Solorzano</h2>
+        <h2>bla bla bla bla bla bla</h2>
+        <h4>bla bla bla bla bla bla</h4>
+      </div>
+    </div>
+  );
+};
