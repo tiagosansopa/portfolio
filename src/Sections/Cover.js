ea5b49037e3e7ec1cccb434e38edb76d943dc9ce
tiagosansopa <tiagosansopa@gmail.com>
2022-07-14 23:05:01 -0600

    add material ui

--- src/Sections/Cover.js
+++ src/Sections/Cover.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { Button, Card, CardContent } from "@mui/material";
 import z from "./z.jpg";
 import "./Cover.css";
 
 export const Cover = () => {
   return (
-    <div className="cover">
-      <div className="card">
+    <Card>
+      <CardContent>
         <img src={z}></img>
-        <h1>This site is under construction</h1>
+        <h1>Santiago Solórzano</h1>
         <h2>come back later</h2>
         <h3>thank you</h3>
-        <h4>uim</h4>
-      </div>
-    </div>
+        <Button variant="contained">UIM</Button>;
+      </CardContent>
+    </Card>
   );
 };
 
